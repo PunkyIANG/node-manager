f86ad8ff3d890237396a6e2aaa0419611d1e2985
zapto123 <tsoorckanukris@gmail.com>
2020-03-01 13:32:54 +0200

    moved files to new repo w/ updated gitignore

---
+++ node test/HelloWorldNode.cs
@@ -0,0 +1,45 @@
+﻿using DynamicData;
+using NodeNetwork.Toolkit.ValueNode;
+using NodeNetwork.ViewModels;
+using NodeNetwork.Views;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace node_test
+{
+    class HelloWorldNode : NodeViewModel
+    {
+        public ValueNodeInputViewModel<string> nameInput { get; }
+        public ValueNodeOutputViewModel<string> textOutput { get; }
+        public HelloWorldNode()
+        {
+            this.Name = "Hello World Node!";
+
+            nameInput = new ValueNodeInputViewModel<string>();
+            {
+                Name = "Name";
+            }
+
+            this.Inputs.Add(nameInput);
+
+            textOutput = new ValueNodeOutputViewModel<string>()
+            {
+                Name = "Text",
+                Value = this.WhenAnyObservable(vm => vm.nameInput.ValueChanged)
+                    .Select(name => $"Hello {name}!")
+            };
+
+            this.Outputs.Add(textOutput);
+        }
+
+        static HelloWorldNode()
+        {
+            Splat.Locator.CurrentMutable.Register(() => new NodeView(), typeof(IViewFor<HelloWorldNode>));
+        }
+    }
+}
